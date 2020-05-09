23ba35613fe639a981a87c8ba8f883294e90c7f2
Andrae J Henry <andrae.henry@icloud.com>
2020-05-09 17:53:43 -0400

    Integrate core data

---
+++ Todoey/Controllers/CatgegoryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CatgegoryViewController.swift
+//  Todoey
+//
+//  Created by Andrae Henry on 5/8/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CatgegoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
---
+++ Todoey/K.swift
@@ -0,0 +1,13 @@
+//
+//  K.swift
+//  Todoey
+//
+//  Created by Andrae Henry on 5/5/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct k {
+   let arrayKey = "TodoListArray"
+}
